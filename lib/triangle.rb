cb7e459709ac67be40eb8186a7ddf04ecba8f62f
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-09-11 20:30:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,16 +7,22 @@ class Triangle
   end
 
   def kind
-    if
-      (@l1==@l2) && (@l2==@l3)
-    :equilateral
-  elsif
-    (@l1==@l2) || (@l2== @l3) || (@l3==@l1)
-    :isosceles
-  else
-    (@l1!=@l2) && (@l2!=@l3) && (@l3!=@l1)
-    :scalene
-  end
+    if (@l1+@l2<@l3)||(@l2+@l3<@l1)||(@l1+@l3<@l2)
+      raise TriangleError
+    elsif
+      @l1==0 ||@l2==0 ||@l3==0
+    else
+        if
+        (@l1==@l2) && (@l2==@l3)
+        :equilateral
+        elsif
+          (@l1==@l2) || (@l2== @l3) || (@l3==@l1)
+          :isosceles
+        else
+          (@l1!=@l2) && (@l2!=@l3) && (@l3!=@l1)
+          :scalene
+      end
+    end
 end
 
   class TriangleError < StandardError
