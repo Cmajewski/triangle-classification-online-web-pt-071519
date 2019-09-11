59566ad593e5a33ce861d1a1bf4ac4d879a8b2c4
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-09-11 20:11:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,24 @@
 class Triangle
   def initialize (l1,l2,l13)
-
+    @l1=l1
+    @l2=l2
+    @l3=l3
   end
+
+  def kind
+    if
+      @l1==@l2 && @l2==@l3
+    :equilateral
+  elsif
+    @l1==@l2 || @l2== @l3 || @l3==@l4
+    :isosceles
+  else
+    :scalene
+
+  end 
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
+end
 end
